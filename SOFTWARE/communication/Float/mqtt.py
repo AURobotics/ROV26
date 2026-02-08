fb19e621a54cf0a034d19fcc7eb37bfb73f7b487
Abdelaziz Islam Galal <abdelaziz.islam.galal@gmail.com>
2026-02-08 17:39:51 +0200

    changed OG mqtt class, publishing uses method in MqttMessage + added explanatory comments

--- SOFTWARE/communication/Float/mqtt.py
+++ SOFTWARE/communication/Float/mqtt.py
@@ -33,8 +33,19 @@ class Mqtt():
             pass
 
 class MqttMessage(ABC):
+    @classmethod
+    def encode(cls, *args) -> str:
+        if len(args) == 1:
+            message = str(args[0])
+        elif len(args) > 1:
+            message = ",".join(str(arg) for arg in args)
+        else:
+            message = ""
+        
+        return message
+
     @abstractmethod
-    def encode(self, data:list):
+    def decode(self, data:list):
         pass
     
 
@@ -43,12 +54,7 @@ class Topic():
         self.topic = topic
         self.mqtt = mqtt_connection
 
-    def publish(self, *args):
-        if len(args) == 1:
-            message = str(args[0])
-        else:
-            message = ",".join(str(arg) for arg in args)
-        
+    def publish(self, message:str):
         self._pub_thread = Thread(target=self._publishing, args=[self.topic, message], daemon=True)
         self._pub_thread.start()
         
@@ -80,54 +86,22 @@ class Topic():
     def _sub_on_message(self, client, userdata, message):
         payload_str: str = message.payload.decode()
         print(f'Received on {message.topic}: {payload_str}') # debug print
-        self.message_handler.encode(payload_str.split(',')) # change the variables in message_handler
+        self.message_handler.decode(payload_str.split(',')) # change the variables in message_handler
 
-# class SubTopic():
-#     def __init__(self, topic, mqtt_connection:Mqtt):
-#         self.client = pahoMC(CallbackAPIVersion.VERSION2)
-#         self.topic = topic #topic name
+"""
+    How does this work?
+    1. We create an Mqtt connection object (mqtt_connection).
+    2. We create a concrete implementation of MqttMessage (TestMessageHandler)
+    3. We create a Topic object with the topic name and the mqtt_connection.
+    4. We subscribe to the topic with our message handler (topic.subscribe(message_handler)).
+    5. We publish messages to the topic using encode method in TestMessageHandler (topic.publish(TestMessageHandler.encode(...))).
+    6. The message handler's decode method is called with the received message, and it updates its internal state (received_data and message_count) accordingly.
 
-#         self.client.on_connect = self._on_connect
-#         self.client.on_message = self._on_message
-#         self.client.connect(mqtt_connection.address, mqtt_connection.port)
-#         self.client.loop_start()
-
-#     def _on_connect(self, client, userdata, flags, reason_code, properties):
-#         if reason_code.is_failure:
-#             print(f'Failed to connect to {self.topic}.') 
-#         else:
-#             client.subscribe(self.topic)
-#             print(f'Subscribed to {self.topic}')
-
-#     def _on_message(self, client, userdata, message):
-#         payload_str: str = message.payload.decode()
-#         print(f'Received on {message.topic}: {payload_str}')
-#         # payload_str.split(',')
-        
-
-# class PubTopic():
-#     def __init__(self, topic, mqtt_connection:Mqtt):
-#         self.mqtt = mqtt_connection
-#         self.topic = topic
-
-#     def publish(self, *args):
-#         if len(args) == 1:
-#             message = str(args[0])
-#         else:
-#             message = ",".join(str(arg) for arg in args)
-        
-#         self._pub_thread = Thread(target=self._publishing, args=[self.topic, message], daemon=True)
-#         self._pub_thread.start()
-        
-#     def _publishing(self, topic, message):    
-#         mqtt_client = self.mqtt
-        
-#         msg = mqtt_client.pub.publish(topic, message)
-#         if mqtt_client.unacked_publish is not None:
-#             mqtt_client.unacked_publish.add(msg.mid)
-#         while len(mqtt_client.unacked_publish):
-#             time.sleep(0.1)
-#         msg.wait_for_publish()
+    summarizing MqttMessage:
+    - MqttMessage is an abstract base class that defines the structure for MQTT messages.
+    - It has a class method "encode" -> use that to encode messages before publishing.
+    - It has an abstract method decode -> this is how message will be decoded when received
+"""
 
 
 if __name__ == "__main__":
@@ -140,7 +114,7 @@ if __name__ == "__main__":
             self.received_data = []
             self.message_count = 0
             
-        def encode(self, data: list):
+        def decode(self, data: list):
             # Store the received data
             self.received_data.append(data)
             self.message_count += 1
@@ -162,18 +136,18 @@ if __name__ == "__main__":
     
     # Test 1: Publish a simple string message
     print("\nTest 1: Publishing single string message")
-    topic.publish('Hello, MQTT!')
+    topic.publish(TestMessageHandler.encode("Hello, MQTT!"))
     time.sleep(1)
     
     # Test 2: Publish multiple arguments (will be joined with commas)
     print("\nTest 2: Publishing multiple arguments")
-    topic.publish('arg1', 42, 3.14, 'last_arg')
+    topic.publish(TestMessageHandler.encode('arg1', 42, 3.14, 'last_arg'))
     time.sleep(1)
     
     # Test 3: Publish multiple messages in a loop
     print("\nTest 3: Publishing multiple messages in loop")
     for i in range(3):
-        topic.publish(f'Message #{i}', f'Value_{i*10}', i*100)
+        topic.publish(TestMessageHandler.encode(f'Message #{i}', f'Value_{i*10}', i*100))
         time.sleep(0.5)
     
     # Wait for all messages to be received
