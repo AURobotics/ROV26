c171d41299b30143023efc91b48ba4fb5559a545
minawashere <minasalama2580@gmail.com>
2026-02-19 03:16:37 +0200

    user defined labels

--- src/firmware/Core/Src/gpio.c
+++ src/firmware/Core/Src/gpio.c
@@ -51,19 +51,19 @@ void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_2|GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14
-                          |GPIO_PIN_15|GPIO_PIN_5, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOB, LED_FLASHER_Pin|MOTOR_GRIPPER_A_Pin|MOTOR_GRIPPER_B_Pin|DCV_1_Pin
+                          |DCV_2_Pin|POWER_RELAY_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pins : PC14 PC15 */
-  GPIO_InitStruct.Pin = GPIO_PIN_14|GPIO_PIN_15;
+  /*Configure GPIO pins : LIMIT_SWITCH_CLOSED_Pin LIMIT_SWITCH_OPEN_Pin */
+  GPIO_InitStruct.Pin = LIMIT_SWITCH_CLOSED_Pin|LIMIT_SWITCH_OPEN_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : PB2 PB12 PB13 PB14
-                           PB15 PB5 */
-  GPIO_InitStruct.Pin = GPIO_PIN_2|GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14
-                          |GPIO_PIN_15|GPIO_PIN_5;
+  /*Configure GPIO pins : LED_FLASHER_Pin MOTOR_GRIPPER_A_Pin MOTOR_GRIPPER_B_Pin DCV_1_Pin
+                           DCV_2_Pin POWER_RELAY_Pin */
+  GPIO_InitStruct.Pin = LED_FLASHER_Pin|MOTOR_GRIPPER_A_Pin|MOTOR_GRIPPER_B_Pin|DCV_1_Pin
+                          |DCV_2_Pin|POWER_RELAY_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
