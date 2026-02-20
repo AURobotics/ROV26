ffd506096d70fd55694c866e4b12522aac3b1632
seif5905 <seifeino2005@gmail.com>
2026-02-20 10:08:51 +0200

    added tabs and fixed camera bugs

---
+++ gui_window.py
@@ -0,0 +1,74 @@
+from PySide6.QtWidgets import QMainWindow, QWidget, QToolBar, QStatusBar, QLabel, QPushButton, QDockWidget, QVBoxLayout, QTabWidget
+from PySide6.QtGui import QAction, QIcon
+from camera_display import Camera
+from PySide6.QtCore import Qt
+from pilot_tab import PilotTab
+from copilot_tab import CoPilotTab
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Main Window")
+        
+        tool_bar = QToolBar("Main ToolBar")
+        self.addToolBar(tool_bar)
+        connect_esp_action = QAction("Connect ESP", self)
+        connect_esp_action.setStatusTip("Connect ESP to App")
+        connect_esp_action.triggered.connect(self.connect_esp)
+        tool_bar.addAction(connect_esp_action)
+
+        connect_controller_action = QAction(QIcon("playstation-controller.webp"), "Connect Controller", self)
+        connect_controller_action.setStatusTip("Connect Controller to App")
+        connect_controller_action.triggered.connect(self.connect_controller)
+        tool_bar.addAction(connect_controller_action)
+
+        tool_bar.addSeparator()
+
+        self.setStatusBar(QStatusBar(self))
+
+        self.sidebar = QDockWidget("Controls", self)
+        self.sidebar.setAllowedAreas(Qt.DockWidgetArea.RightDockWidgetArea)
+        #self.sidebar.resize(400, self.height())
+        
+        button1 = QPushButton("One")
+        button2 = QPushButton("Two")
+        label1 = QLabel("A Label")
+
+        sidebar_layout = QVBoxLayout()
+        sidebar_layout.addWidget(button1)
+        sidebar_layout.addWidget(button2)
+        sidebar_layout.addWidget(label1)
+
+        sidebar_widget = QWidget()
+        sidebar_widget.setLayout(sidebar_layout)
+
+        self.sidebar.setWidget(sidebar_widget)
+
+        self.addDockWidget(Qt.DockWidgetArea.RightDockWidgetArea, self.sidebar)
+        self.resizeDocks([self.sidebar], [250], Qt.Horizontal)
+        self.sidebar.hide()
+
+        self.camera = Camera(0)
+        
+        self.pilot_tab = PilotTab(self.toggle_sidebar, self.camera, self.camera, self.camera)
+        self.copilot_tab = CoPilotTab(self.toggle_sidebar, self.camera)
+
+        tab_widget = QTabWidget()
+        tab_widget.addTab(self.pilot_tab, "Pilot")
+        tab_widget.addTab(self.copilot_tab, "Co-Pilot")
+
+        self.setCentralWidget(tab_widget)
+
+
+
+    def connect_esp(self):
+        print("ESP Connected")
+
+    def connect_controller(self):
+        print("Controller Connected")
+
+    def toggle_sidebar(self, checked):
+        self.sidebar.setVisible(checked)
+          
---
+++ pilot_tab.py
@@ -0,0 +1,36 @@
+from PySide6.QtWidgets import QWidget, QGridLayout, QPushButton, QLabel
+from PySide6.QtGui import QPixmap
+from camera_display import CameraDisplay
+
+class PilotTab(QWidget):
+    def __init__(self, toggle_sidebar, cam1,cam2,cam3):
+        super().__init__()
+
+        grid_layout = QGridLayout()
+
+        self.camera1 = CameraDisplay(cam1)
+        self.camera2 = CameraDisplay(cam2)
+        self.camera3 = CameraDisplay(cam3)
+
+        self.image1 = QLabel()
+        self.image1.setPixmap(QPixmap("pic.png"))
+
+        self.image2 = QLabel()
+        self.image2.setPixmap(QPixmap("pic.png"))
+
+        self.image3 = QLabel()
+        self.image3.setPixmap(QPixmap("pic.png"))
+
+        self.sidebar_button = QPushButton("Control")
+        self.sidebar_button.setCheckable(True)
+        self.sidebar_button.clicked.connect(toggle_sidebar)
+
+        grid_layout.addWidget(self.camera1,0,0)
+        grid_layout.addWidget(self.camera2,0,1)
+        grid_layout.addWidget(self.camera3,0,2)
+        grid_layout.addWidget(self.image1,1,0)
+        grid_layout.addWidget(self.image2,1,1)
+        grid_layout.addWidget(self.image3,1,2)
+        grid_layout.addWidget(self.sidebar_button,2,2)
+
+        self.setLayout(grid_layout)
