13de5bd9bb03471140472d3069c5e1ae71f355f2
minawashere <minasalama2580@gmail.com>
2026-02-22 21:42:15 +0200

    added sensor drivers

--- src/firmware/Core/lib/ms5611/ms5611.h
+++ src/firmware/Core/lib/ms5611/ms5611.h
@@ -1,10 +1,23 @@
-//
-// Created by lolo on 2/22/26.
-//
+#pragma once
+#include <stdint.h>
 
-#ifndef FIRMWARE_MS5611_H
-#define FIRMWARE_MS5611_H
+class MS5611
+{
+public:
 
-class ms5611 {};
+  bool begin() ;
+  void calibrateSurface () ;
+  float getTemperature() ;
+  float getPressure() ;
+  float getDepth() ;
+private:
 
-#endif // FIRMWARE_MS5611_H
+  uint16_t C1 ,C2 , C3 , C4 , C5 , C6 ;
+  float surfacePressure ;
+  float density = 1024.0 ;
+  void readCalibrationData() ;
+  void reset() ;
+  uint32_t readADC() ;
+};
+
+//https://makerselectronics.com/wp-content/uploads/2025/09/ENG_DS_MS5611-01BA03_B3.pdf
