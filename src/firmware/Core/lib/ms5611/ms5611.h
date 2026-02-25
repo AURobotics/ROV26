8b61ae97bdde8d69b64dd2da63a75effd9153ece
rowan mokhtar <rowanmokhtar6@gmail.com>
2026-02-25 04:39:47 +0200

    HAL ms

--- src/firmware/Core/lib/ms5611/ms5611.h
+++ src/firmware/Core/lib/ms5611/ms5611.h
@@ -1,23 +1,32 @@
-#pragma once
+#ifndef MS5611_H
+#define MS5611_H
+#include "stm32f4xx_hal.h"
 #include <stdint.h>
 
+#define i2cAddr (0x77 << 1 )
 class MS5611
-{
+{  
 public:
+    
+   MS5611(I2C_HandleTypeDef *hi2c) ;
 
-  bool begin() ;
-  void calibrateSurface () ;
-  float getTemperature() ;
-  float getPressure() ;
-  float getDepth() ;
+   bool begin() ;
+   void calibrateSurface () ;
+    float getTemperature() ;
+    float getPressure() ;
+    float getDepth() ;
 private:
+   I2C_HandleTypeDef *_hi2c;
+uint16_t C1 ,C2 , C3 , C4 , C5 , C6 ;
+float surfacePressure ;
+float density = 1024.0 ; 
+void readCalibrationData() ;
+void reset() ;
+void sendCmd(uint8_t cmd) ; 
+uint32_t readADC() ;
+uint16_t readPROM( I2C_HandleTypeDef *hi2c, uint8_t addr) ;
 
-  uint16_t C1 ,C2 , C3 , C4 , C5 , C6 ;
-  float surfacePressure ;
-  float density = 1024.0 ;
-  void readCalibrationData() ;
-  void reset() ;
-  uint32_t readADC() ;
 };
+#endif
 
-//https://makerselectronics.com/wp-content/uploads/2025/09/ENG_DS_MS5611-01BA03_B3.pdf
+// https://makerselectronics.com/wp-content/uploads/2025/09/ENG_DS_MS5611-01BA03_B3.pdf
