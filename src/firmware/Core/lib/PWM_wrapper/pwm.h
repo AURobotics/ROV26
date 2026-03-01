4675b8afc28ab29d358d94a0cf6f6c0c24a34d6d
minawashere <minasalama2580@gmail.com>
2026-03-01 23:03:33 +0200

    added motor class

--- src/firmware/Core/lib/PWM_wrapper/pwm.h
+++ src/firmware/Core/lib/PWM_wrapper/pwm.h
@@ -6,7 +6,12 @@ class PWM {
     uint32_t channel;
 
 public:
-    PWM(TIM_HandleTypeDef* timer, uint32_t ch);
+    explicit constexpr PWM(TIM_HandleTypeDef* timer, uint32_t ch);
+    PWM(const PWM&) = delete; // copy constructor
+    PWM& operator=(const PWM&) = delete;
+
+    PWM(PWM&&) = default; // move constructor
+    PWM& operator=(PWM&&) = default;
     void start() const;
     void set_duty(uint16_t duty) const; // 0–255
 };
