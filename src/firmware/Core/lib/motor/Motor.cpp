1230bb66dcac01624f9a9f5ff2becbd7382086b1
dania ahmed <ahmeddanya074@gmail.com>
2026-03-02 01:32:04 +0200

    gripper motor zabtnah yarab fok el dy2a

--- src/firmware/Core/lib/motor/Motor.cpp
+++ src/firmware/Core/lib/motor/Motor.cpp
@@ -4,13 +4,22 @@
 float constrain(const float x, const float a, const float b) { return x > b ? b : x < a ? a : x; }
 
 Motor::Motor(const PWM& p1, const PWM& p2) : pwm1(p1), pwm2(p2) {}
+Motor::Motor(std::function<void(float)> customHandler) :
+    pwm1(nullptr), pwm2(nullptr), handler(customHandler) {}
 
 void Motor::setup() const {
+    if (handler)
+        return;
     pwm1.start();
     pwm2.start();
 }
 
 void Motor::move(float speed) const {
+    if (handler) {
+        handler(speed);
+        return;
+    }
+
     speed = constrain(speed, -1, 1);
 
     const auto duty = static_cast<uint16_t>(std::fabs(speed) * 255);
@@ -25,7 +34,12 @@ void Motor::move(float speed) const {
     }
 }
 
+
 void Motor::stop() const {
+    if (handler) {
+        handler(0);
+        return;
+    }
     pwm1.set_duty(255);
     pwm2.set_duty(255);
 }
