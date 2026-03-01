63cc78c6b5db594d29e6160d141108c68976eb46
dania ahmed <ahmeddanya074@gmail.com>
2026-03-02 01:32:04 +0200

    gripper motor zabtnah yarab fok el dy2a

--- src/firmware/Core/lib/motor/Motor.h
+++ src/firmware/Core/lib/motor/Motor.h
@@ -1,11 +1,14 @@
 #pragma once
+#include <functional>
 #include "pwm.h"
 
 class Motor {
     const PWM &pwm1, &pwm2;
+    std::function<void(float)> handler;
 
 public:
-    Motor(const PWM& p1,  const PWM& p2);
+    Motor(const PWM& p1, const PWM& p2);
+    Motor(std::function<void(float)> Handler);
     void setup() const;
     void move(float speed) const;
     void stop() const;
