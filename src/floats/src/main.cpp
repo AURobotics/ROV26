7da21dfea890c98d62474558fdcfb27e55e981e8
zyman1000 <zyman510@gmail.com>
2026-03-03 23:42:54 +0200

    added interfacing with motor via PID control

--- src/floats/src/main.cpp
+++ src/floats/src/main.cpp
@@ -1,4 +1,9 @@
 #include <Arduino.h>
+#include <TMCStepper.h>
+#include <HardwareSerial.h>
+
+#define R_SENSE 0.11f //to be changed
+#define SERIAL Serial1
 
 class PID{
     public:
@@ -14,6 +19,11 @@ class PID{
         float current_integral = 0;
         float prev_error = 0;
         float prev_time = 0;
+        float prev_D = 0;
+        float set_point;
+        float calculate_error(float current_reading){
+            return this->set_point - current_reading;
+        }
         float calculate_PID(float error, float time_stamp){
             float P = this->Kp * error;
             this->current_integral += error * (time_stamp - this->prev_time);
@@ -28,9 +38,14 @@ class PID{
                 I = this->current_integral * this->Ki;
             }
             //anti windup ends here
-            float D = Kd * (error - this->prev_error) / (time_stamp - this->prev_time);
+            float D;
+            if(time_stamp - this->prev_time == 0) //make sure to not divide by 0
+                D = prev_D;
+            else
+                D = Kd * (error - this->prev_error) / (time_stamp - this->prev_time);
             this->prev_error = error;
             this->prev_time = time_stamp;
+            this->prev_D = D;
             float PID = P+I+D;
             if(PID > this->max_motor_output){
                 PID = this->max_motor_output;
@@ -42,10 +57,38 @@ class PID{
         }
 };
 
+TMC2208Stepper driver = TMC2208Stepper(&SERIAL, R_SENSE);
+PID pid = PID(0,0,0,0); //to be added: PID parameters + max motor output
+float time;
+bool hold_position = false;
 void setup() {
-// write your initialization code here
+  // put your setup code here, to run once:
+  SERIAL.begin(115200);
+  driver.begin();
+  //what about sense resistors?
+  driver.toff(5);
+  driver.pwm_autoscale(true);
+  driver.pdn_disable(true); //allow UART to control (not only configure)
+  // driver.I_scale_analog(false); //use internal reference derived from 5VOUT
+  driver.mstep_reg_select(true);
+
+  pid.set_point = -2.5; //initial set point
+  time = millis(); //start time
 }
 
 void loop() {
-// write your code here
+  // put your main code here, to run repeatedly:
+  if(hold_position && millis() - time > 1000 * 30){ //if we have been holding position for 30 seconds, we flip direction
+    pid.set_point = -2.5 - pid.set_point; //flip motor direction after being stable for 30 seconds
+    pid.current_integral = 0; //reset integral to help change direction faster
+    hold_position = false;
+  }
+  float height = get_height(); //get height from pressure sensor
+  float error = pid.calculate_error(height);
+  if(!hold_position && error < 0.3){ //error less than 30 cm
+    hold_position = true; //start holding position
+    time = millis();
+  }
+  float signal = pid.calculate_PID(error, millis());
+  driver.VACTUAL(signal);
 }
