1bdd913f91874efd50a0d702adcabc41cd06649f
minawashere <minasalama2580@gmail.com>
2026-03-04 02:40:22 +0200

    5lst el code el tante dania kan el mafrod t3mlo

--- src/firmware/Core/lib/motor/Motor.cpp
+++ src/firmware/Core/lib/motor/Motor.cpp
@@ -5,46 +5,54 @@
 float constrain(const float x, const float a, const float b) { return x > b ? b : x < a ? a : x; }
 
 
-
 // Motor::Motor(PWM& p1, PWM& p2) : pwm1(p1), pwm2(p2) {}
 // Motor::Motor(std::function<void(float)> customHandler) : handler(std::move(customHandler)) {}
 
 void Motor::setup() const {
-    if (handler)
-
+    switch (this->handler_type) {
+    case HandlerType::FUNCTION :
         return;
-    pwm1.start();
-    pwm2.start();
+    case HandlerType::PWM :
+        Handler.pwm_handler.p1->start();
+        Handler.pwm_handler.p2->start();
+        break;
+    }
 }
 
 void Motor::move(float speed) const {
-    if (handler) {
-        handler(speed);
-        return;
-    }
-
     speed = constrain(speed, -1, 1);
-
-    const auto duty = static_cast<uint16_t>(std::fabs(speed) * 255);
-
-    if (speed > 0) {
-        pwm1.set_duty(duty);
-        pwm2.set_duty(0);
-    }
-    else if (speed < 0) {
-        pwm1.set_duty(0);
-        pwm2.set_duty(duty);
+    switch (this->handler_type) {
+    case HandlerType::FUNCTION :
+        Handler.handler_function(speed);
+        return;
+    case HandlerType::PWM :
+        const auto pwm1 = this->Handler.pwm_handler.p1;
+        const auto pwm2 = this->Handler.pwm_handler.p2;
+        speed = constrain(speed, -1, 1);
+        const auto duty = static_cast<uint16_t>(std::fabs(speed) * 255);
+        if (speed > 0) {
+            pwm1->set_duty(duty);
+            pwm2->set_duty(0);
+        }
+        else if (speed <= 0) {
+            pwm1->set_duty(0);
+            pwm2->set_duty(duty);
+        }
     }
 }
 
 
 void Motor::stop() const {
-    if (handler) {
-        handler(0);
+    switch (this->handler_type) {
+    case HandlerType::FUNCTION :
+        move(0);
         return;
+    case HandlerType::PWM :
+        const auto pwm1 = this->Handler.pwm_handler.p1;
+        const auto pwm2 = this->Handler.pwm_handler.p2;
+        pwm1->set_duty(255);
+        pwm2->set_duty(255);
     }
-    pwm1.set_duty(255);
-    pwm2.set_duty(255);
 }
 
 void Motor::move_motor(Motor motors[8], float speeds[8]) {
