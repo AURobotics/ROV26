c4ae9e670e5c37e00db8feb334138204abcd3e3b
Youssra Farghaly <youssramhfarghaly@gmail.com>
2026-03-13 14:11:09 +0200

    enter dfu mode men pi

---
+++ build/CMakeFiles/firmware.dir/DependInfo.cmake
@@ -0,0 +1,79 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "C:/Users/youss/OneDrive/Desktop/AUR/ROV26/firmware/startup_stm32f401xc.s" "C:/Users/youss/OneDrive/Desktop/AUR/ROV26/build/CMakeFiles/firmware.dir/startup_stm32f401xc.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32F401xC"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "C:/Users/youss/OneDrive/Desktop/AUR/ROV26/firmware/Core/lib/pid"
+  "C:/Users/youss/OneDrive/Desktop/AUR/ROV26/firmware/Core/lib/bno055"
+  "C:/Users/youss/OneDrive/Desktop/AUR/ROV26/firmware/Core/lib/ms5611"
+  "C:/Users/youss/OneDrive/Desktop/AUR/ROV26/firmware/Core/lib/motor"
+  "C:/Users/youss/OneDrive/Desktop/AUR/ROV26/firmware/Core/lib/controller"
+  "C:/Users/youss/OneDrive/Desktop/AUR/ROV26/firmware/Core/lib/kinematics"
+  "C:/Users/youss/OneDrive/Desktop/AUR/ROV26/firmware/Core/lib/usb_comms"
+  "C:/Users/youss/OneDrive/Desktop/AUR/ROV26/firmware/Core/lib/i2c_wrapper"
+  "C:/Users/youss/OneDrive/Desktop/AUR/ROV26/firmware/Core/lib/PWM_wrapper"
+  "C:/Users/youss/OneDrive/Desktop/AUR/ROV26/firmware/Core/Inc"
+  "C:/Users/youss/OneDrive/Desktop/AUR/ROV26/firmware/cmake/stm32cubemx/../../Core/Inc"
+  "C:/Users/youss/OneDrive/Desktop/AUR/ROV26/firmware/cmake/stm32cubemx/../../USB_DEVICE/App"
+  "C:/Users/youss/OneDrive/Desktop/AUR/ROV26/firmware/cmake/stm32cubemx/../../USB_DEVICE/Target"
+  "C:/Users/youss/OneDrive/Desktop/AUR/ROV26/firmware/cmake/stm32cubemx/../../Drivers/STM32F4xx_HAL_Driver/Inc"
+  "C:/Users/youss/OneDrive/Desktop/AUR/ROV26/firmware/cmake/stm32cubemx/../../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "C:/Users/youss/OneDrive/Desktop/AUR/ROV26/firmware/cmake/stm32cubemx/../../Middlewares/ST/STM32_USB_Device_Library/Core/Inc"
+  "C:/Users/youss/OneDrive/Desktop/AUR/ROV26/firmware/cmake/stm32cubemx/../../Middlewares/ST/STM32_USB_Device_Library/Class/CDC/Inc"
+  "C:/Users/youss/OneDrive/Desktop/AUR/ROV26/firmware/cmake/stm32cubemx/../../Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "C:/Users/youss/OneDrive/Desktop/AUR/ROV26/firmware/cmake/stm32cubemx/../../Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/youss/OneDrive/Desktop/AUR/ROV26/firmware/Core/Src/adc.c" "CMakeFiles/firmware.dir/Core/Src/adc.c.obj" "gcc" "CMakeFiles/firmware.dir/Core/Src/adc.c.obj.d"
+  "C:/Users/youss/OneDrive/Desktop/AUR/ROV26/firmware/Core/Src/gpio.c" "CMakeFiles/firmware.dir/Core/Src/gpio.c.obj" "gcc" "CMakeFiles/firmware.dir/Core/Src/gpio.c.obj.d"
+  "C:/Users/youss/OneDrive/Desktop/AUR/ROV26/firmware/Core/Src/i2c.c" "CMakeFiles/firmware.dir/Core/Src/i2c.c.obj" "gcc" "CMakeFiles/firmware.dir/Core/Src/i2c.c.obj.d"
+  "C:/Users/youss/OneDrive/Desktop/AUR/ROV26/firmware/Core/Src/stm32f4xx_hal_msp.c" "CMakeFiles/firmware.dir/Core/Src/stm32f4xx_hal_msp.c.obj" "gcc" "CMakeFiles/firmware.dir/Core/Src/stm32f4xx_hal_msp.c.obj.d"
+  "C:/Users/youss/OneDrive/Desktop/AUR/ROV26/firmware/Core/Src/stm32f4xx_it.c" "CMakeFiles/firmware.dir/Core/Src/stm32f4xx_it.c.obj" "gcc" "CMakeFiles/firmware.dir/Core/Src/stm32f4xx_it.c.obj.d"
+  "C:/Users/youss/OneDrive/Desktop/AUR/ROV26/firmware/Core/Src/syscalls.c" "CMakeFiles/firmware.dir/Core/Src/syscalls.c.obj" "gcc" "CMakeFiles/firmware.dir/Core/Src/syscalls.c.obj.d"
+  "C:/Users/youss/OneDrive/Desktop/AUR/ROV26/firmware/Core/Src/sysmem.c" "CMakeFiles/firmware.dir/Core/Src/sysmem.c.obj" "gcc" "CMakeFiles/firmware.dir/Core/Src/sysmem.c.obj.d"
+  "C:/Users/youss/OneDrive/Desktop/AUR/ROV26/firmware/Core/Src/tim.c" "CMakeFiles/firmware.dir/Core/Src/tim.c.obj" "gcc" "CMakeFiles/firmware.dir/Core/Src/tim.c.obj.d"
+  "C:/Users/youss/OneDrive/Desktop/AUR/ROV26/firmware/USB_DEVICE/App/usb_device.c" "CMakeFiles/firmware.dir/USB_DEVICE/App/usb_device.c.obj" "gcc" "CMakeFiles/firmware.dir/USB_DEVICE/App/usb_device.c.obj.d"
+  "C:/Users/youss/OneDrive/Desktop/AUR/ROV26/firmware/USB_DEVICE/App/usbd_cdc_if.c" "CMakeFiles/firmware.dir/USB_DEVICE/App/usbd_cdc_if.c.obj" "gcc" "CMakeFiles/firmware.dir/USB_DEVICE/App/usbd_cdc_if.c.obj.d"
+  "C:/Users/youss/OneDrive/Desktop/AUR/ROV26/firmware/USB_DEVICE/App/usbd_desc.c" "CMakeFiles/firmware.dir/USB_DEVICE/App/usbd_desc.c.obj" "gcc" "CMakeFiles/firmware.dir/USB_DEVICE/App/usbd_desc.c.obj.d"
+  "C:/Users/youss/OneDrive/Desktop/AUR/ROV26/firmware/USB_DEVICE/Target/usbd_conf.c" "CMakeFiles/firmware.dir/USB_DEVICE/Target/usbd_conf.c.obj" "gcc" "CMakeFiles/firmware.dir/USB_DEVICE/Target/usbd_conf.c.obj.d"
+  "C:/Users/youss/OneDrive/Desktop/AUR/ROV26/firmware/Core/Src/main.cpp" "CMakeFiles/firmware.dir/Core/Src/main.cpp.obj" "gcc" "CMakeFiles/firmware.dir/Core/Src/main.cpp.obj.d"
+  "C:/Users/youss/OneDrive/Desktop/AUR/ROV26/firmware/Core/lib/PWM_wrapper/pwm.cpp" "CMakeFiles/firmware.dir/Core/lib/PWM_wrapper/pwm.cpp.obj" "gcc" "CMakeFiles/firmware.dir/Core/lib/PWM_wrapper/pwm.cpp.obj.d"
+  "C:/Users/youss/OneDrive/Desktop/AUR/ROV26/firmware/Core/lib/bno055/bno055.cpp" "CMakeFiles/firmware.dir/Core/lib/bno055/bno055.cpp.obj" "gcc" "CMakeFiles/firmware.dir/Core/lib/bno055/bno055.cpp.obj.d"
+  "C:/Users/youss/OneDrive/Desktop/AUR/ROV26/firmware/Core/lib/controller/Controller.cpp" "CMakeFiles/firmware.dir/Core/lib/controller/Controller.cpp.obj" "gcc" "CMakeFiles/firmware.dir/Core/lib/controller/Controller.cpp.obj.d"
+  "C:/Users/youss/OneDrive/Desktop/AUR/ROV26/firmware/Core/lib/i2c_wrapper/i2c_wrapper.cpp" "CMakeFiles/firmware.dir/Core/lib/i2c_wrapper/i2c_wrapper.cpp.obj" "gcc" "CMakeFiles/firmware.dir/Core/lib/i2c_wrapper/i2c_wrapper.cpp.obj.d"
+  "C:/Users/youss/OneDrive/Desktop/AUR/ROV26/firmware/Core/lib/kinematics/Kinematics.cpp" "CMakeFiles/firmware.dir/Core/lib/kinematics/Kinematics.cpp.obj" "gcc" "CMakeFiles/firmware.dir/Core/lib/kinematics/Kinematics.cpp.obj.d"
+  "C:/Users/youss/OneDrive/Desktop/AUR/ROV26/firmware/Core/lib/motor/Motor.cpp" "CMakeFiles/firmware.dir/Core/lib/motor/Motor.cpp.obj" "gcc" "CMakeFiles/firmware.dir/Core/lib/motor/Motor.cpp.obj.d"
+  "C:/Users/youss/OneDrive/Desktop/AUR/ROV26/firmware/Core/lib/ms5611/ms5611.cpp" "CMakeFiles/firmware.dir/Core/lib/ms5611/ms5611.cpp.obj" "gcc" "CMakeFiles/firmware.dir/Core/lib/ms5611/ms5611.cpp.obj.d"
+  "C:/Users/youss/OneDrive/Desktop/AUR/ROV26/firmware/Core/lib/pid/PID.cpp" "CMakeFiles/firmware.dir/Core/lib/pid/PID.cpp.obj" "gcc" "CMakeFiles/firmware.dir/Core/lib/pid/PID.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
