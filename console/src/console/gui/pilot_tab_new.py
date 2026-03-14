068930b551e29d36e0a95dab353560f9b8b916e2
Mohamed-Eltahhan <mohamed.eltahhan1@gmail.com>
2026-03-14 23:04:46 +0200

    Added a leakage display

--- console/src/console/gui/pilot_tab_new.py
+++ console/src/console/gui/pilot_tab_new.py
@@ -1,5 +1,6 @@
 from PySide6.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QSizePolicy
 from console.gui.camera_display import CameraDisplay
+from console.gui.leakage_display import LeakageDisplay
 from console.gui.model.orientation_data import OrientationData
 from console.gui.model.sensors import Sensors
 from console.gui.model.thruster_status import ThrusterStatus
@@ -30,18 +31,23 @@ class PilotTab2(QWidget):
         #2. Bottom Widgets
         self._widget_layout = QHBoxLayout()
 
-        #2.1 Thrusters
+        #2.1 Leakage
+        self.leakage_widget = LeakageDisplay()
+        self.leakage_widget.setSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Preferred)
+        self._widget_layout.addWidget(self.leakage_widget, stretch=0)
+
+        #2.2 Thrusters
         self.thruster_layout_widget = StatusWidget(ThrusterStatus(Sensors(self._comms)), "thrusterLayout.qml")
         self.thruster_layout_widget.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Preferred)
         self._widget_layout.addWidget(self.thruster_layout_widget, stretch=0)
 
-        #2.2 Depth
+        #2.3 Depth
         self.depth_widget = StatusWidget(OrientationData(Sensors(self._comms)), "depth.qml")
         self.depth_widget.setSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Preferred)
         self._widget_layout.addWidget(self.depth_widget, stretch=0)
 
 
-        #2.3 Orientation
+        #2.4 Orientation
         self._orientation_layout = QVBoxLayout()
         self.compass_widget = StatusWidget(OrientationData(Sensors(self._comms)), "compassWidget.qml")
         self.pitch_roll_widget = StatusWidget(OrientationData(Sensors(self._comms)), "pitchRoll.qml")
