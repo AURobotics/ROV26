63733e140d9a71a49318d9e35d08e244f259a604
nada-osama-a <nadaharraz73@gmail.com>
2026-03-19 02:26:56 +0200

    Add Blender projects and scripts

---
+++ Research & Testing/software/vision/Batch-Files/light.bat
@@ -0,0 +1,3 @@
+@echo off
+"C:\Program Files\Blender Foundation\Blender 4.5\blender.exe" -b "C:\Users\nadah\OneDrive\Desktop\light.blend" -P "C:\Users\nadah\OneDrive\Desktop\BlenderScripts\light.py"
+pause
