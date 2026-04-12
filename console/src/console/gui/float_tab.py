6fbdca61cc654da0f6e2e98c77d0b68dc774f0db
Mohamed-Eltahhan <mohamed.eltahhan1@gmail.com>
2026-04-12 16:31:19 +0200

    Sidebar styling + prelimenary Float tab

---
+++ console/src/console/gui/float_tab.py
@@ -0,0 +1,6 @@
+from PySide6.QtWidgets import QWidget
+
+class FloatTab(QWidget):
+    def __init__(self, parent: QWidget | None = None):
+        super().__init__(parent)
+        ... # Placeholder for FloatTab implementation
