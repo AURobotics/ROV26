cdee75089f5add70cbd4cd8a6817f836a8f6a900
Abdelaziz Islam Galal <abdelaziz.islam.galal@gmail.com>
2026-04-12 06:49:51 +0200

    added company number recieve end

--- float/software/comms/main.py
+++ float/software/comms/main.py
@@ -18,11 +18,24 @@ def main():
             self.status = message.payload.decode()
             print(f"Received status message: {self.status}")
             self.status_received = True
+
+    class float_comapny_number_handler(mqtt_message):
+        def __init__(self):
+            super().__init__()
+            self.company_number = None
+        
+        def decode(self, message):
+            self.company_number = message.payload.decode()
+            print(f"Received company number: {self.company_number}")
             
     float_status_topic = topic(SECONDARY_TOPIC_NAME, mqtt_client)
     float_status_hander = status_handler()
     float_status_topic.subscribe(float_status_hander)
     
+    float_company_number_topic = topic(f"float/data/credential", mqtt_client)
+    float_company_number_handler = float_comapny_number_handler()
+    float_company_number_topic.subscribe(float_company_number_handler)
+
     file_receiver_instance = file_receiver(mqtt_client, f"{MAIN_TOPIC_NAME}", crc32=False)
 
     while not file_receiver_instance.is_complete:
