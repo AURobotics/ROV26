f141ade59e9cd22e0532f604d840e4a919926f0c
Abdelaziz Islam Galal <abdelaziz.islam.galal@gmail.com>
2026-04-13 17:56:32 +0200

    added float-gui

--- console/src/console/gui/float_tab.py
+++
@@ -1,6 +0,0 @@
-from PySide6.QtWidgets import QWidget
-
-class FloatTab(QWidget):
-    def __init__(self, parent: QWidget | None = None):
-        super().__init__(parent)
-        ... # Placeholder for FloatTab implementation
