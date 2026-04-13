1c7b4e59a743b33297f0075279f4c10bdf7289f4
Youssef Samy <office.youssefkassem@gmail.com>
2026-04-13 09:54:49 +0200

    One leakage sensor

--- console/src/console/gui/leakage_display.py
+++ console/src/console/gui/leakage_display.py
@@ -4,6 +4,7 @@ from PySide6.QtCore import QTimer
 
 from random import randint
 
+
 class LeakageDisplay(QWidget):
     def __init__(self, parent: QWidget | None = None):
         super().__init__(parent)
@@ -18,7 +19,8 @@ class LeakageDisplay(QWidget):
         self._sensor1 = QProgressBar()
         self._sensor1.setTextVisible(False)
         self._sensor1.setFixedHeight(8)
-        self._sensor1.setStyleSheet('''
+        self._sensor1.setStyleSheet(
+            """
             QProgressBar {
                 border-radius: 4px;
                 background-color: grey;
@@ -26,45 +28,26 @@ class LeakageDisplay(QWidget):
             }
             QProgressBar::chunk {
                 border-radius: 4px;
-                background-color: ''' + self._colour1 + ';}'
-                                    )
+                background-color: """
+            + self._colour1
+            + ";}"
+        )
         self._sensor1.setRange(0, 100)
         self._sensor1.setValue(0)
-        self._sensor1.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Preferred)
+        self._sensor1.setSizePolicy(
+            QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Preferred
+        )
         self._layout.addWidget(self._sensor1, 0, 0)
 
-        self._label1 = QLabel('temp1')
+        self._label1 = QLabel("temp1")
         self._label1.setFont(font)
-        self._label1.setSizePolicy(QSizePolicy.Policy.Maximum, QSizePolicy.Policy.Maximum)
+        self._label1.setSizePolicy(
+            QSizePolicy.Policy.Maximum, QSizePolicy.Policy.Maximum
+        )
         self._layout.addWidget(self._label1, 0, 1)
 
-        self._colour2 = "#62c1e5"
-
-        self._sensor2 = QProgressBar()
-        self._sensor2.setTextVisible(False)
-        self._sensor2.setFixedHeight(8)
-        self._sensor2.setStyleSheet('''
-            QProgressBar {
-                border-radius: 4px;
-                background-color: grey;
-                padding: 0px;
-            }
-            QProgressBar::chunk {
-                border-radius: 4px;
-                background-color: ''' + self._colour2 + ';}'
-                                    )
-        self._sensor2.setRange(0, 100)
-        self._sensor2.setValue(40)
-        self._sensor2.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Preferred)
-        self._layout.addWidget(self._sensor2, 1, 0)
-
-        self._label2 = QLabel('temp2')
-        self._label2.setFont(font)
-        self._label2.setSizePolicy(QSizePolicy.Policy.Maximum, QSizePolicy.Policy.Maximum)
-        self._layout.addWidget(self._label2, 1, 1)
-
         metrics = QFontMetrics(font)
-        max_label_width = metrics.horizontalAdvance('100%')
+        max_label_width = metrics.horizontalAdvance("100%")
         self._layout.setColumnMinimumWidth(1, max_label_width + 10)
         self._layout.setColumnStretch(1, 0)
 
@@ -73,10 +56,10 @@ class LeakageDisplay(QWidget):
         self._timer.start(100)
 
     def updateSensors(self):
-        new_value = max(0, min(100, self._sensor1.value() + randint(-5,5)))
+        new_value = max(0, min(100, self._sensor1.value() + randint(-5, 5)))
         if new_value != self._sensor1.value():
             self._sensor1.setValue(new_value)
-            self._label1.setText(f'{self._sensor1.value()}%')
+            self._label1.setText(f"{self._sensor1.value()}%")
 
             if self._sensor1.value() < 25:
                 new_colour = "#62c1e5"
@@ -89,7 +72,8 @@ class LeakageDisplay(QWidget):
 
             if new_colour != self._colour1:
                 self._colour1 = new_colour
-                self._sensor1.setStyleSheet('''
+                self._sensor1.setStyleSheet(
+                    """
                     QProgressBar {
                         border-radius: 4px;
                         background-color: grey;
@@ -97,35 +81,10 @@ class LeakageDisplay(QWidget):
                     }
                     QProgressBar::chunk {
                         border-radius: 4px;
-                        background-color: ''' + self._colour1 + ';}'
-                                            )
-        
-        new_value = max(0, min(100, self._sensor2.value() + randint(-5,5)))
-        if new_value != self._sensor2.value():
-            self._sensor2.setValue(new_value)
-            self._label2.setText(f'{self._sensor2.value()}%')
-
-            if self._sensor2.value() < 25:
-                new_colour = "#62c1e5"
-            elif self._sensor2.value() < 50:
-                new_colour = "#F2D06B"
-            elif self._sensor2.value() < 75:
-                new_colour = "#F4A261"
-            else:
-                new_colour = "#E76F51"
-
-            if new_colour != self._colour2:
-                self._colour2 = new_colour
-                self._sensor2.setStyleSheet('''
-                    QProgressBar {
-                        border-radius: 4px;
-                        background-color: grey;
-                        padding: 0px;
-                    }
-                    QProgressBar::chunk {
-                        border-radius: 4px;
-                        background-color: ''' + self._colour2 + ';}'
-                                            )
+                        background-color: """
+                    + self._colour1
+                    + ";}"
+                )
 
     def hideEvent(self, event):
         super().hideEvent(event)
