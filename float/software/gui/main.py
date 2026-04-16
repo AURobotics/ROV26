bb95b1fc526fd620c157272672ac1ede6e4ea616
Abdelaziz Islam Galal <abdelaziz.islam.galal@gmail.com>
2026-04-16 18:06:08 +0200

    single app with no tabs

--- float/software/gui/main.py
+++ float/software/gui/main.py
@@ -2,14 +2,13 @@ import sys
 
 from PySide6.QtWidgets import QApplication
 
-from gui.float_tab import DataViewerTab
 from gui.main_window import DemoWindow
 
 def run_gui():
     app = QApplication(sys.argv)
     app.setStyle("Fusion")
 
-    win = DemoWindow(DataViewerTab())
+    win = DemoWindow()
     win.show()
     sys.exit(app.exec())
     print("hello")
